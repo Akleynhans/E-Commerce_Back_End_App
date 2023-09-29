5b3481b0ec8d6c09aa891e1c74ba26c25fa4d566
Amy Kleynhans <amykleynhans@yahoo.com>
2023-09-29 08:03:33 -0600

    populated index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'catagory_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'catagory_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'Product_Tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'Tags_product'
+});
 
 module.exports = {
   Product,
